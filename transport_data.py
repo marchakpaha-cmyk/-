462bb0b11e4aff105c4f63269e43d58b4bfac1d1
marchakpaha-cmyk <marchakpaha@gmail.com>
2026-06-26 22:24:44 +0300

    Add transport data for Kyiv public transport system
    0.1 метро+скоростной трамвай

---
+++ transport_data.py
@@ -0,0 +1,188 @@
+TRANSPORT_DB = {
+    "Киев": {
+        "Метрополитен": {
+            "Святошинско-Броварская (М1)": {
+                "color": "#E52B20", "status": "active", "stations": {
+                    "Академгородок": {"coords": [50.4647, 30.3551], "photo": "", "status": "active"},
+                    "Житомирская": {"coords": [50.4561, 30.3644], "photo": "", "status": "active"},
+                    "Святошин": {"coords": [50.4578, 30.3917], "photo": "", "status": "active"},
+                    "Нивки": {"coords": [50.4586, 30.4139], "photo": "", "status": "active"},
+                    "Берестейская": {"coords": [50.4591, 30.4203], "photo": "", "status": "active"},
+                    "Шулявская": {"coords": [50.4550, 30.4456], "photo": "", "status": "active"},
+                    "Политехнический институт": {"coords": [50.4508, 30.4661], "photo": "", "status": "active"},
+                    "Вокзальная": {"coords": [50.4417, 30.4881], "photo": "", "status": "active"},
+                    "Университет": {"coords": [50.4442, 30.5061], "photo": "", "status": "active"},
+                    "Театральная": {"coords": [50.4453, 30.5181], "photo": "", "status": "active"},
+                    "Крещатик": {"coords": [50.4472, 30.5233], "photo": "", "status": "active"},
+                    "Арсенальная": {"coords": [50.4444, 30.5456], "photo": "", "status": "active"},
+                    "Днепр": {"coords": [50.4411, 30.5586], "photo": "", "status": "active"},
+                    "Гидропарк": {"coords": [50.4458, 30.5769], "photo": "", "status": "active"},
+                    "Левобережная": {"coords": [50.4519, 30.5983], "photo": "", "status": "active"},
+                    "Дарница": {"coords": [50.4558, 30.6125], "photo": "", "status": "active"},
+                    "Черниговская": {"coords": [50.4611, 30.6314], "photo": "", "status": "active"},
+                    "Лесная": {"coords": [50.4644, 30.6447], "photo": "", "status": "active"}
+                }
+            },
+            "Оболонско-Теремковская (М2)": {
+                "color": "#0072CE", "status": "active", "stations": {
+                    "Героев Днепра": {"coords": [50.5225, 30.4986], "photo": "", "status": "active"},
+                    "Минская": {"coords": [50.5122, 30.4983], "photo": "", "status": "active"},
+                    "Оболонь": {"coords": [50.5014, 30.4981], "photo": "", "status": "active"},
+                    "Почайна": {"coords": [50.4864, 30.4981], "photo": "", "status": "active"},
+                    "Тараса Шевченко": {"coords": [50.4764, 30.5028], "photo": "", "status": "active"},
+                    "Контрактовая площадь": {"coords": [50.4653, 30.5169], "photo": "", "status": "active"},
+                    "Почтовая площадь": {"coords": [50.4594, 30.5247], "photo": "", "status": "active"},
+                    "Площадь Независимости": {"coords": [50.4497, 30.5244], "photo": "", "status": "active"},
+                    "Площадь Украинских Героев": {"coords": [50.4397, 30.5167], "photo": "", "status": "active"},
+                    "Олимпийская": {"coords": [50.4325, 30.5158], "photo": "", "status": "active"},
+                    "Дворец Украина": {"coords": [50.4211, 30.5208], "photo": "", "status": "active"},
+                    "Лыбедская": {"coords": [50.4139, 30.5247], "photo": "", "status": "active"},
+                    "Демеевская": {"coords": [50.4047, 30.5169], "photo": "", "status": "active"},
+                    "Голосеевская": {"coords": [50.3975, 30.5083], "photo": "", "status": "active"},
+                    "Васильковская": {"coords": [50.3933, 30.4878], "photo": "", "status": "active"},
+                    "Выставочный центр": {"coords": [50.3825, 30.4775], "photo": "", "status": "active"},
+                    "Ипподром": {"coords": [50.3764, 30.4692], "photo": "", "status": "active"},
+                    "Теремки": {"coords": [50.3672, 30.4542], "photo": "", "status": "active"}
+                }
+            },
+            "Сырецко-Печерская (М3)": {
+                "color": "#00933C", "status": "active", "stations": {
+                    "Проспект Правды": {"coords": [50.5052, 30.4192], "photo": "", "status": "project"},
+                    "Мостицкая": {"coords": [50.4936, 30.4298], "photo": "", "status": "project"}, 
+                    "Сырец": {"coords": [50.4761, 30.4308], "photo": "", "status": "active"},
+                    "Дорогожичи": {"coords": [50.4736, 30.4492], "photo": "", "status": "active"},
+                    "Лукьяновская": {"coords": [50.4622, 30.4819], "photo": "", "status": "active"},
+                    "Львовская брама": {"coords": [50.4543, 30.5034], "photo": "", "status": "project"},
+                    "Золотые ворота": {"coords": [50.4483, 30.5133], "photo": "", "status": "active"},
+                    "Дворец спорта": {"coords": [50.4383, 30.5211], "photo": "", "status": "active"},
+                    "Кловская": {"coords": [50.4369, 30.5317], "photo": "", "status": "active"},
+                    "Печерская": {"coords": [50.4272, 30.5392], "photo": "", "status": "active"},
+                    "Зверинецкая": {"coords": [50.4172, 30.5450], "photo": "", "status": "active"},
+                    "Выдубичи": {"coords": [50.4019, 30.5606], "photo": "", "status": "active"},
+                    "Славутич": {"coords": [50.3942, 30.5978], "photo": "", "status": "active"},
+                    "Осокорки": {"coords": [50.3956, 30.6161], "photo": "", "status": "active"},
+                    "Позняки": {"coords": [50.3981, 30.6347], "photo": "", "status": "active"},
+                    "Харьковская": {"coords": [50.4008, 30.6522], "photo": "", "status": "active"},
+                    "Вырлица": {"coords": [50.4028, 30.6658], "photo": "", "status": "active"},
+                    "Бориспольская": {"coords": [50.4033, 30.6828], "photo": "", "status": "active"},
+                    "Красный хутор": {"coords": [50.4089, 30.6953], "photo": "", "status": "active"}
+                }
+            },
+            "Подольско-Выгуровская (М4)": {
+                "color": "#F3A000", "status": "project", "stations": {
+                    "Глубочицкая": {"coords": [50.4622, 30.4819], "photo": "", "status": "project"},
+                    "Подольская": {"coords": [50.4764, 30.5028], "photo": "", "status": "project"},
+                    "Судостроительная": {"coords": [50.4728, 30.5306], "photo": "", "status": "project"},
+                    "Труханов остров": {"coords": [50.4739, 30.5444], "photo": "", "status": "project"},
+                    "Залив Десёнка": {"coords": [50.4761, 30.5603], "photo": "", "status": "project"},
+                    "Радужная": {"coords": [50.4800, 30.5750], "photo": "", "status": "project"},
+                    "Бульвар Перова": {"coords": [50.4925, 30.5947], "photo": "", "status": "project"},
+                    "Проспект Ватутина": {"coords": [50.5042, 30.5969], "photo": "", "status": "project"},
+                    "Улица Цветаевой": {"coords": [50.5164, 30.6056], "photo": "", "status": "project"},
+                    "Милославская": {"coords": [50.5256, 30.6014], "photo": "", "status": "project"}
+                }
+            }
+        },
+        "Трамвай": {
+            "Скоростной №1": {
+                "color": "#E60000", "status": "active", "stations": {
+                    "Старовокзальная": {"coords": [50.4436, 30.4906], "photo": "", "status": "active"},
+                    "Площадь Победы": {"coords": [50.4464, 30.4925], "photo": "", "status": "active"},
+                    "Воздухофлотская": {"coords": [50.4485, 30.4819], "photo": "", "status": "historical"},
+                    "Политехническая": {"coords": [50.4497, 30.4678], "photo": "", "status": "active"},
+                    "Полевая": {"coords": [50.4439, 30.4503], "photo": "", "status": "active"},
+                    "Индустриальная": {"coords": [50.4419, 30.4403], "photo": "", "status": "active"},
+                    "Национальный авиационный университет": {"coords": [50.4372, 30.4286], "photo": "", "status": "active"},
+                    "Героев Севастополя": {"coords": [50.4300, 30.4183], "photo": "", "status": "active"},
+                    "Ивана Лепсе": {"coords": [50.4244, 30.4086], "photo": "", "status": "active"},
+                    "Семьи Сосниных": {"coords": [50.4194, 30.3992], "photo": "", "status": "active"},
+                    "Гната Юры": {"coords": [50.4239, 30.3800], "photo": "", "status": "active"},
+                    "Улица Генерала Потапова": {"coords": [50.4158, 30.3775], "photo": "", "status": "active"},
+                    "Бульвар Кольцова": {"coords": [50.4094, 30.3803], "photo": "", "status": "active"},
+                    "Улица Зодчих": {"coords": [50.4036, 30.3881], "photo": "", "status": "active"},
+                    "Улица Жолудева": {"coords": [50.4168, 30.3976], "photo": "", "status": "active"},
+                    "Улица Булгакова": {"coords": [50.4116, 30.4042], "photo": "", "status": "active"},
+                    "Михайловская Борщаговка": {"coords": [50.4031, 30.3952], "photo": "", "status": "active"}
+                }
+            },
+            "Скоростной №2": {
+                "color": "#D32F2F", "status": "active", "stations": {
+                    "Кольцевая дорога": {"coords": [50.4133, 30.3586], "photo": "", "status": "active"},
+                    "Ромена Роллана": {"coords": [50.4197, 30.3694], "photo": "", "status": "active"},
+                    "Гната Юры": {"coords": [50.4239, 30.3800], "photo": "", "status": "active"},
+                    "Улица Генерала Потапова": {"coords": [50.4158, 30.3775], "photo": "", "status": "active"},
+                    "Бульвар Кольцова": {"coords": [50.4094, 30.3803], "photo": "", "status": "active"},
+                    "Улица Зодчих": {"coords": [50.4036, 30.3881], "photo": "", "status": "active"},
+                    "Улица Жолудева": {"coords": [50.4168, 30.3976], "photo": "", "status": "active"},
+                    "Улица Булгакова": {"coords": [50.4116, 30.4042], "photo": "", "status": "active"},
+                    "Михайловская Борщаговка": {"coords": [50.4031, 30.3952], "photo": "", "status": "active"}
+                    "Проспект Королёва": {"coords": [50.4132, 30.4121], "photo": "", "status": "active"},
+
+                }
+            },
+            "Скоростной №3": {
+                "color": "#C62828", "status": "active", "stations": {
+                    "Старовокзальная": {"coords": [50.4436, 30.4906], "photo": "", "status": "active"},
+                    "Площадь Победы": {"coords": [50.4464, 30.4925], "photo": "", "status": "active"},
+                    "Воздухофлотская": {"coords": [50.4485, 30.4819], "photo": "", "status": "historical"},
+                    "Политехническая": {"coords": [50.4497, 30.4678], "photo": "", "status": "active"},
+                    "Полевая": {"coords": [50.4439, 30.4503], "photo": "", "status": "active"},
+                    "Индустриальная": {"coords": [50.4419, 30.4403], "photo": "", "status": "active"},
+                    "Национальный авиационный университет": {"coords": [50.4372, 30.4286], "photo": "", "status": "active"},
+                    "Героев Севастополя": {"coords": [50.4300, 30.4183], "photo": "", "status": "active"},
+                    "Ивана Лепсе": {"coords": [50.4244, 30.4086], "photo": "", "status": "active"},
+                    "Семьи Сосниных": {"coords": [50.4194, 30.3992], "photo": "", "status": "active"},
+                    "Гната Юры": {"coords": [50.4239, 30.3800], "photo": "", "status": "active"},
+                    "Ромена Роллана": {"coords": [50.4197, 30.3694], "photo": "", "status": "active"},
+                    "Кольцевая дорога": {"coords": [50.4133, 30.3586], "photo": "", "status": "active"}
+                }
+            },
+            "Скоростной №4": {
+                "color": "#B71C1C", "status": "active", "stations": {
+                    "Милославская": {"coords": [50.5250, 30.6017], "photo": "", "status": "active"},
+                    "Улица Цветаевой": {"coords": [50.5164, 30.6056], "photo": "", "status": "active"},
+                    "Улица Сабурова": {"coords": [50.5056, 30.6100], "photo": "", "status": "active"},
+                    "Улица Драйзера": {"coords": [50.4944, 30.6142], "photo": "", "status": "active"},
+                    "Улица Каштановая": {"coords": [50.4872, 30.6114], "photo": "", "status": "active"},
+                    "Станция Проспект Ватутина": {"coords": [50.4814, 30.5958], "photo": "", "status": "active"},
+                    "Троещина-2": {"coords": [50.4789, 30.5606], "photo": "", "status": "active"}
+                }
+            },
+            "Скоростной №5": {
+                "color": "#9E1C1C", "status": "active", "stations": {
+                    "Улица Сабурова": {"coords": [50.5056, 30.6100], "photo": "", "status": "active"},
+                    "Улица Цветаевой": {"coords": [50.5164, 30.6056], "photo": "", "status": "active"},
+                    "Милославская": {"coords": [50.5250, 30.6017], "photo": "", "status": "active"},
+                    "Улица Цветаевой (вниз)": {"coords": [50.5164, 30.6056], "photo": "", "status": "active"},
+                    "Улица Сабурова (вниз)": {"coords": [50.5056, 30.6100], "photo": "", "status": "active"},
+                    "Улица Драйзера": {"coords": [50.4944, 30.6142], "photo": "", "status": "active"},
+                    "Улица Каштановая": {"coords": [50.4872, 30.6114], "photo": "", "status": "active"},
+                    "Станция Проспект Ватутина": {"coords": [50.4814, 30.5958], "photo": "", "status": "active"},
+                    "Троещина-2": {"coords": [50.4789, 30.5606], "photo": "", "status": "active"}
+                }
+            },
+            # Каркасы для остальных маршрутов (вписывай станции сюда)
+            "№8 Лесная - Позняки": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№11 Контрактовая - Иорданская": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№12 Контрактовая - Пуща Водица": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№14 Контрактовая - Автогенный завод": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№15 Старовокзальная - Автогенный завод": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№16 Контрактовая - Героев Днепра": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№17 Пуща Водица - Улица Иорданская": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№18 Контрактовая - Старовокзальная": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№19 Контрактовая - пл. Шевченко": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№22 ЗЖБК - б-р Перова": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№23 ДВРЗ - Алишера Навои": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№25 Позняки ЗЖБК": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№27 Милославская - Позняки": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№28 Милославская - м. Лесная": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№29 м. Лесная - м. Бориспольская": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№32 м. Лесная - ДВРЗ": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№33 ДВРЗ - Сабурова": {"color": "#D32F2F", "status": "active", "stations": {}},
+            "№35 м.Лесная - Сабурова": {"color": "#D32F2F", "status": "active", "stations": {}}
+        },
+        "Троллейбус": {
+            # Базовая инфраструктура готова. Наполняй маршрутами по аналогии с трамваем.
+        }
+    }
+}
